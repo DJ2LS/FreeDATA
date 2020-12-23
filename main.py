17294069c9e3378d8636f5a63146e93d45e0956a
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2020-12-23 18:25:50 +0100

    added cli parameters

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Created on Tue Dec 22 16:58:45 2020
 
 import socketserver
 import threading
-
+import argparse
 
 import tnc
 import static
@@ -18,12 +18,27 @@ import modem
 modem = modem.RF()
 
 
+#--------------------------------------------GET PARAMETER INPUTS  
+parser = argparse.ArgumentParser(description='Simons TEST TNC')
+parser.add_argument('--rx', dest="audio_input_device", default=False, help="sound card for listening.", type=int)
+parser.add_argument('--tx', dest="audio_output_device", default=False, help="sound card for transmitting.", type=int)
+parser.add_argument('--port', dest="socket_port", default=9000, help="Set the port, the socket is listening on.", type=int)  
+
+args = parser.parse_args()
+
+
+static.AUDIO_INPUT_DEVICE = args.audio_input_device
+static.AUDIO_OUTPUT_DEVICE = args.audio_output_device
+static.PORT = args.socket_port
+
+
+#--------------------------------------------START AUDIO THREAD  
 static.MODEM_RECEIVE = True    
 audio_receiver_thread = threading.Thread(target=modem.Receive, name="Audio Listener")
 audio_receiver_thread.start()
 
 
-
+#--------------------------------------------START SERVER  
 try:
     server = socketserver.TCPServer((static.HOST, static.PORT), tnc.TCPRequestHandler)
     server.serve_forever()
