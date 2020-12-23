d5922281a1815e6d82e106c360dfcafbb9f9cf01
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2020-12-23 17:49:25 +0100

    Add files via upload
    Client for testing

---
+++ socketclient.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 11 21:53:35 2020
+
+@author: parallels
+"""
+
+import socket
+import sys
+
+HOST, PORT = "localhost", 3000
+data = " ".join(sys.argv[1:])
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(bytes(data + "\n", "utf-8"))
+
+    # Receive data from the server and shut down
+    received = str(sock.recv(1024), "utf-8")
+
+print("Sent:     {}".format(data))
+print("Received: {}".format(received))
