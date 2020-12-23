fe25ce68b8bdd5dbcf5128b7b4a705d991f3f6b5
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2020-12-23 17:48:54 +0100

    Add files via upload

---
+++ static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 23 11:13:57 2020
+
+@author: parallels
+"""
+
+# Modem States
+MODEM_RECEIVE = True
+
+
+# FreeDV Defaults
+FREEDV_MODE = 12
+
+# Server Defaults
+HOST = "localhost"
+PORT = 3000
+
+
+#AUdio Defaults
+DEFAULT_FRAMES = 1024
+AUDIO_INPUT_DEVICE = 1
+AUDIO_OUTPUT_DEVICE = 1
+TX_SAMPLE_STATE = None
+RX_SAMPLE_STATE = None
+AUDIO_SAMPLE_RATE = 8000
+MODEM_SAMPLE_RATE = 8000
+AUDIO_FRAMES_PER_BUFFER = 1024
+AUDIO_CHANNELS = 1
--- tnc.py
+++ tnc.py
@@ -1,34 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 11 20:06:55 2020
 
-@author: DJ2LS
-"""
 
-import logging
 import socketserver
-import freedv
-import commands
+import threading
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
+import modem
+import static
+
+modem = modem.RF()
 
 
+class TCPRequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
+        
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
+        
+     
+        # interrupt listening loop "while true" by setting MODEM_RECEIVE to False
+        if len(self.data) > 0:
+            static.MODEM_RECEIVE = False
+        
+        
         print("{} wrote:".format(self.client_address[0]))
         print(self.data)
+        
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
         
-        if self.data == b'TEST':
-            logging.info("DER TEST KLAPPT! HIER KOMMT DER COMMAND PARSER HIN!")
+        #if self.data == b'TEST':
+            #logging.info("DER TEST KLAPPT! HIER KOMMT DER COMMAND PARSER HIN!")
 
-# BROADCAST PARSER ###########################################################
-        #if self.data == b'BC':
+# BROADCAST PARSER  -----------------------------------------------------------
+
         if self.data.startswith(b'BC:'):
+            static.MODEM_RECEIVE = False
+            print(static.MODEM_RECEIVE)
+            
             data = self.data.split(b'BC:')
-            commands.TX.Broadcast(bytes(data[1]),bytes(data[1]))
-            #commands.TX.Broadcast(data,data)
+            daten = modem.Transmit(data[1])
+            
+            static.MODEM_RECEIVE = True
+            print(static.MODEM_RECEIVE)
