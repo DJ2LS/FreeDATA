0dccb0a3d4204eb88dd6e994f400b864331d7667
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2021-03-12 14:14:08 +0100

    PEP8 cleaner

---
+++ tools/cleanup.sh
@@ -0,0 +1,3 @@
+#autopep8 --in-place --select W291,W293,W391,E231 --ignore E501 ../modem.py ../data_handler.py ../main.py ../sock.py ../static.py ../helpers.py
+autopep8 --in-place --ignore E501 ../modem.py ../data_handler.py ../main.py ../sock.py ../static.py ../helpers.py
+
