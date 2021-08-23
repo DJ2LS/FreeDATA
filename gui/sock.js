831da389461059512f4d1ee0b6400ade1cd638ce
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2021-08-23 15:55:33 +0200

    typo

--- gui/sock.js
+++ gui/sock.js
@@ -139,7 +139,7 @@ client.on('data', function(data) {
         
         if (data['COMMAND'] == 'RX_BUFFER') {
             let Data = {
-                data : data['DATAARRAY'],
+                data : data['DATA-ARRAY'],
             };
             console.log(Data)
             ipcRenderer.send('request-update-rx-buffer', Data);
