34788be3ba436c7378d2549b4c4a20ba6f8034b6
dj2ls <dj2ls@protonmail.com>
2021-12-12 22:00:50 +0100

    updated codec2 header file

--- test/codec2.py
+++ test/codec2.py
@@ -6,7 +6,7 @@ from ctypes import *
 import sys
 
 
-print("loading module", file=sys.stderr)
+print("loading codec2 module", file=sys.stderr)
 
 
 # LOAD FREEDV
@@ -27,6 +27,15 @@ api.freedv_nin.restype = c_int
 api.freedv_rawdatarx.argtype = [c_void_p, c_char_p, c_char_p]
 api.freedv_rawdatarx.restype = c_int
 
+api.freedv_rawdatatx.argtype = [c_void_p, c_char_p, c_char_p]
+api.freedv_rawdatatx.restype = c_int
+
+api.freedv_rawdatapostambletx.argtype = [c_void_p, c_char_p, c_char_p]
+api.freedv_rawdatapostambletx.restype = c_int
+
+api.freedv_rawdatapreambletx.argtype = [c_void_p, c_char_p, c_char_p]
+api.freedv_rawdatapreambletx.restype = c_int
+
 api.freedv_get_n_max_modem_samples.argtype = [c_void_p]
 api.freedv_get_n_max_modem_samples.restype = c_int
 
@@ -35,3 +44,13 @@ api.freedv_set_frames_per_burst.restype = c_void_p
       
 api.freedv_get_rx_status.argtype = [c_void_p]
 api.freedv_get_rx_status.restype = c_int  
+
+api.freedv_get_n_tx_postamble_modem_samples.argtype = [c_void_p]
+api.freedv_get_n_tx_postamble_modem_samples.restype = c_int 
+
+api.freedv_get_n_tx_preamble_modem_samples.argtype = [c_void_p]
+api.freedv_get_n_tx_preamble_modem_samples.restype = c_int 
+
+api.freedv_get_n_tx_modem_samples.argtype = [c_void_p]
+api.freedv_get_n_tx_modem_samples.restype = c_int 
+
