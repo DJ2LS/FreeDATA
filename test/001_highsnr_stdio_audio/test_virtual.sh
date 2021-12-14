eeb221a31bf59f239204bdac05ce0cfc3ca048bd
drowe67 <david@rowetel.com>
2021-12-15 06:47:36 +1030

    simpler script

--- test/001_highsnr_stdio_audio/test_virtual.sh
+++ test/001_highsnr_stdio_audio/test_virtual.sh
@@ -18,8 +18,8 @@ RX_LOG=$(mktemp)
 # make sure all child processes are killed when we exit
 trap 'jobs -p | xargs -r kill' EXIT
 
-python3 test_rx.py --mode datac0 --frames 2 --bursts 1 --audiodev 4 --debug 2>&1 | tee ${RX_LOG} &
+python3 test_rx.py --mode datac0 --frames 2 --bursts 1 --audiodev 4 --debug &
+rx_pid=$!
 sleep 1
 python3 test_tx.py --mode datac0 --frames 2 --bursts 1 --audiodev 5
-
-tail -f ${RX_LOG} | sed '/RECEIVED BURSTS/ q'
+wait ${rx_pid}
