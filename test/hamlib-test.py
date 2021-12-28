5a15064a353b28b14309256889de02e1914d9e19
dj2ls <dj2ls@protonmail.com>
2021-12-28 17:36:50 +0100

    slight cleanup

--- test/hamlib-test.py
+++ test/hamlib-test.py
@@ -41,17 +41,6 @@ class RETCODE(Enum):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 libname = pathlib.Path("../tnc/lib/hamlib/linux/libhamlib.so")
 hamlib = ctypes.CDLL(libname)
 
@@ -83,16 +72,7 @@ class STATE(ctypes.Structure):
 
 class MY_RIG(ctypes.Structure):
     _fields_ = [
-        #("Nc", ctypes.c_int),
-        #("snr_est", ctypes.c_float),
-        #("rx_symbols", (ctypes.c_float * MODEM_STATS_NR_MAX)*MODEM_STATS_NC_MAX),
-        #("nr", ctypes.c_int),
-        #("sync", ctypes.c_int),
-        #("foff", ctypes.c_float),
-        #("rx_timing", ctypes.c_float),
-        #("clock_offset", ctypes.c_float),
-        #("sync_metric", ctypes.c_float),
-        ("pathname", ctypes.c_uchar),
+        ("pathname", ctypes.c_char),
         ("state", STATE),
 
     ]
@@ -115,7 +95,7 @@ model = 3085 #3085 = ICOM 6 = DUMMY
 my_rig = MY_RIG()
 my_rig.state.rigport.parm.serial.data_bits = 7
 
-#rig = hamlib.rig_init(my_rig)
+rig = hamlib.rig_init(my_rig)
 
 #hamlib.serial_setup()
 #hamlib.serial_open('dev12')
