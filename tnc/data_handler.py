bc1b01c340ff61736060bb55a682fd3d52313e61
dj2ls <dj2ls@protonmail.com>
2022-01-07 18:26:01 +0100

    fixed message transfer

--- tnc/data_handler.py
+++ tnc/data_handler.py
@@ -373,7 +373,7 @@ class DATA():
                     
                 # if datatype is a file, we append to RX_MSG_BUFFER, which contains messages only            
                 if rawdata["dt"] == "m":
-                    static.RX_MSG_BUFFER.append([static.DXCALLSIGN,static.DXGRID,int(time.time()), complete_data_frame])
+                    static.RX_MSG_BUFFER.append([static.DXCALLSIGN,static.DXGRID,int(time.time()), data_frame])
                     #structlog.get_logger("structlog").debug("RECEIVED MESSAGE --> MOVING DATA TO MESSAGE BUFFER")
                 
                 
