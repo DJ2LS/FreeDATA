cc46b13982986682710e9cbaaf3100cfb0210811
dj2ls <dj2ls@protonmail.com>
2022-03-11 21:03:25 +0100

    updated version information

--- tnc/static.py
+++ tnc/static.py
@@ -8,7 +8,7 @@ Here we are saving application wide variables and stats, which have to be access
 Not nice, suggestions are appreciated :-) 
 """
 
-VERSION = '0.0.1-alpha'
+VERSION = '0.1.0-alpha'
 
 # DAEMON
 DAEMONPORT = 3001
