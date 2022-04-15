9f9b5e98e2508f8c5468739f6fff70125a2320a5
Paul Kronenwetter <kronenpj@gmail.com>
2022-04-15 15:24:12 -0400

    Remove errant inclusion of NOCALL ping logging.

--- tnc/data_handler.py
+++ tnc/data_handler.py
@@ -1483,11 +1483,6 @@ class DATA():
         valid, mycallsign = helpers.check_callsign(self.mycallsign, data_in[1:3])
         if not valid:
             # PING packet not for me.
-            # But is it a "NO-CALL" PING? Check against an 'empty' callsign.
-            valid, _ = helpers.check_callsign(bytes(), data_in[1:3])
-            if valid:
-                structlog.get_logger("structlog").info("[TNC] PING HEARD [" + "(NOCALL)" + "] <<< [" + str(static.DXCALLSIGN, 'utf-8') + "]", snr=static.SNR )
-            # Either way, skip the remainder of received_ping.
             return
         
         structlog.get_logger("structlog").info("[TNC] PING REQ [" + str(mycallsign, 'utf-8') + "] <<< [" + str(static.DXCALLSIGN, 'utf-8') + "]", snr=static.SNR )
