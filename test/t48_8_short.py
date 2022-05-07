6556e78f64fd289c4a8a5b3e50963d098986640f
Paul Kronenwetter <kronenpj@gmail.com>
2022-05-07 11:35:33 -0400

    Clean up two existing tests.
    Adapted both tests to pytest and maintained
    compatibility with existing ctest method.
    Tweaked CMakeLists.txt .

--- test/t48_8_short.py
+++ test/t48_8_short.py
@@ -13,88 +13,108 @@
 #
 # They should sound like clean sine waves
 
-import ctypes
-from ctypes import *
-import pathlib
-import argparse
+# pylint: disable=global-statement, invalid-name, unused-import
+
+import os
 import sys
-sys.path.insert(0,'..')
-from tnc import codec2
+
+import codec2
 import numpy as np
+import pytest
 
 # dig some constants out
 FDMDV_OS_48 = codec2.api.FDMDV_OS_48
-FDMDV_OS_TAPS_48K = codec2.api.FDMDV_OS_TAPS_48K 
+FDMDV_OS_TAPS_48K = codec2.api.FDMDV_OS_TAPS_48K
 FDMDV_OS_TAPS_48_8K = codec2.api.FDMDV_OS_TAPS_48_8K
 
-N8       = int(180)                   # processing buffer size at 8 kHz
-N48      = int(N8*FDMDV_OS_48)        # processing buffer size at 48 kHz
-MEM8     = FDMDV_OS_TAPS_48_8K        # 8kHz signal filter memory
-MEM48    = FDMDV_OS_TAPS_48K          # 48kHz signal filter memory
-FRAMES   = int(50)                    # number of frames to test
-FS8      = 8000
-FS48     = 48000
-AMP      = 16000                      # sine wave amplitude
-FTEST8   = 800                        # input test frequency at FS=8kHz
-FINTER48 = 10000                      # interferer frequency at FS=48kHz
+N8 = 180  # processing buffer size at 8 kHz
+N48 = N8 * FDMDV_OS_48  # processing buffer size at 48 kHz
+MEM8 = FDMDV_OS_TAPS_48_8K  # 8kHz signal filter memory
+MEM48 = FDMDV_OS_TAPS_48K  # 48kHz signal filter memory
+FRAMES = 50  # number of frames to test
+FS8 = 8000
+FS48 = 48000
+AMP = 16000  # sine wave amplitude
+FTEST8 = 800  # input test frequency at FS=8kHz
+FINTER48 = 10000  # interferer frequency at FS=48kHz
 
 # Due to the design of these resamplers, the processing buffer (at 8kHz)
 # must be an integer multiple of oversampling ratio
 assert N8 % FDMDV_OS_48 == 0
 
-# time indexes, we advance every frame
-t = 0
-t1 = 0
 
-# output files to listen to/evaluate result
-fin8 = open("in8.raw", mode='wb')
-f48 = open("out48.raw", mode='wb')
-fout8 = open("out8.raw", mode='wb')
+def test_resampler():
+    """
+    Test for the codec2 audio resampling routine
+    """
+    # time indexes, we advance every frame
+    t = 0
+    t1 = 0
 
-resampler = codec2.resampler()
+    # output files to listen to/evaluate result
+    with open("in8.raw", mode="wb") as fin8:
+        with open("out48.raw", mode="wb") as f48:
+            with open("out8.raw", mode="wb") as fout8:
+                resampler = codec2.resampler()
 
-for f in range(FRAMES):
+                # Generate FRAMES of a sine wave
+                for _ in range(FRAMES):
+                    # Primary sine wave, which the down-sampling filter should retain.
+                    sine_in8k = (
+                        AMP * np.cos(2 * np.pi * np.arange(t, t + N8) * FTEST8 / FS8)
+                    ).astype(np.int16)
+                    t += N8
+                    sine_in8k.tofile(fin8)
 
-    sine_in8k = (AMP*np.cos(2*np.pi*np.arange(t,t+N8)*FTEST8/FS8)).astype(np.int16)
-    t += N8
-    sine_in8k.tofile(fin8)
+                    sine_out48k = resampler.resample8_to_48(sine_in8k)
+                    sine_out48k.tofile(f48)
 
-    sine_out48k = resampler.resample8_to_48(sine_in8k)
-    sine_out48k.tofile(f48)
-    
-    # add interfering sine wave (down sampling filter should remove)
-    sine_in48k = (sine_out48k + (AMP/2)*np.cos(2*np.pi*np.arange(t1,t1+N48)*FINTER48/FS48)).astype(np.int16)
-    t1 += N48
+                    # Add an interfering sine wave, which the down-sampling filter should (mostly) remove
+                    sine_in48k = (
+                        sine_out48k
+                        + (AMP / 2)
+                        * np.cos(2 * np.pi * np.arange(t1, t1 + N48) * FINTER48 / FS48)
+                    ).astype(np.int16)
+                    t1 += N48
 
-    sine_out8k = resampler.resample48_to_8(sine_in48k)
-    sine_out8k.tofile(fout8)
-      
-fin8.close()
-f48.close()
-fout8.close()
+                    sine_out8k = resampler.resample48_to_8(sine_in48k)
+                    sine_out8k.tofile(fout8)
 
-# Automated test evaluation --------------------------------------------
+    # os.unlink("out48.raw")
 
-# The input and output signals will not be time aligned due to the filter
-# delays, so compare the magnitude spectrum
+    # Automated test evaluation --------------------------------------------
 
-in8k = np.fromfile("in8.raw", dtype=np.int16)
-out8k = np.fromfile("out8.raw", dtype=np.int16)
-assert len(in8k) == len(out8k)
+    # The input and output signals will not be time aligned due to the filter
+    # delays, so compare the magnitude spectrum
 
-n = len(in8k)
+    # Read the raw audio files
+    in8k = np.fromfile("in8.raw", dtype=np.int16)
+    out8k = np.fromfile("out8.raw", dtype=np.int16)
+    assert len(in8k) == len(out8k)
+    # os.unlink("in8.raw")
+    # os.unlink("out8.raw")
 
-h = np.hanning(len(in8k))
-S1 = np.abs(np.fft.fft(in8k * h))
-S2 = np.abs(np.fft.fft(out8k * h))
+    # Apply hanning filter to raw input data samples
+    h = np.hanning(len(in8k))
+    S1 = np.abs(np.fft.fft(in8k * h))
+    S2 = np.abs(np.fft.fft(out8k * h))
 
-error = S1-S2
-error_energy = np.dot(error,error)
-ratio = error_energy/np.dot(S1,S1)
-ratio_dB = 10*np.log10(ratio);
-print("ratio_dB: %4.2f" % (ratio_dB));
-threshdB = -40
-if ratio_dB < threshdB:
-    print("PASS")
-else:
-    print("FAIL")
+    # Calculate the ratio between signal and noise (error energy).
+    error = S1 - S2
+    error_energy = np.dot(error, error)
+    ratio = error_energy / np.dot(S1, S1)
+    ratio_dB = 10 * np.log10(ratio)
+
+    # Establish -40.0 as the noise ratio ceiling
+    threshdB = -40.0
+    print(f"ratio_dB: {ratio_dB:4.2}" % (ratio_dB))
+    assert ratio_dB < threshdB
+
+
+if __name__ == "__main__":
+    # Run pytest with the current script as the filename.
+    ecode = pytest.main(["-v", sys.argv[0]])
+    if ecode == 0:
+        print("PASS")
+    else:
+        print("FAIL")
