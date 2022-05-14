840847f3de0318d2045f6f9942bc7553e5621ea6
Paul Kronenwetter <kronenpj@gmail.com>
2022-05-14 10:02:56 -0400

    Minor cleanups.

--- test/test_callback_rx_outside.py
+++ test/test_callback_rx_outside.py
@@ -6,42 +6,56 @@ Created on Wed Dec 23 07:04:24 2020
 @author: DJ2LS
 """
 
-import ctypes
-from ctypes import *
-import pathlib
-import pyaudio
-import sys
-import logging
-import time
-import threading
-import sys
 import argparse
+import ctypes
+import sys
+import time
+
 import numpy as np
-sys.path.insert(0,'..')
+import pyaudio
+
+sys.path.insert(0, "..")
 from tnc import codec2
 
-#--------------------------------------------GET PARAMETER INPUTS
-parser = argparse.ArgumentParser(description='FreeDATA audio test')
-parser.add_argument('--bursts', dest="N_BURSTS", default=1, type=int)
-parser.add_argument('--framesperburst', dest="N_FRAMES_PER_BURST", default=1, type=int)
-parser.add_argument('--mode', dest="FREEDV_MODE", type=str, choices=['datac0', 'datac1', 'datac3'])
-parser.add_argument('--audiodev', dest="AUDIO_INPUT_DEVICE", default=-1, type=int,
-                    help="audio device number to use, use -2 to automatically select a loopback device")
-parser.add_argument('--debug', dest="DEBUGGING_MODE", action="store_true")
-parser.add_argument('--timeout', dest="TIMEOUT", default=10, type=int, help="Timeout (seconds) before test ends")
-parser.add_argument('--list', dest="LIST", action="store_true", help="list audio devices by number and exit")
+# --------------------------------------------GET PARAMETER INPUTS
+parser = argparse.ArgumentParser(description="FreeDATA audio test")
+parser.add_argument("--bursts", dest="N_BURSTS", default=1, type=int)
+parser.add_argument("--framesperburst", dest="N_FRAMES_PER_BURST", default=1, type=int)
+parser.add_argument(
+    "--mode", dest="FREEDV_MODE", type=str, choices=["datac0", "datac1", "datac3"]
+)
+parser.add_argument(
+    "--audiodev",
+    dest="AUDIO_INPUT_DEVICE",
+    default=-1,
+    type=int,
+    help="audio device number to use, use -2 to automatically select a loopback device",
+)
+parser.add_argument("--debug", dest="DEBUGGING_MODE", action="store_true")
+parser.add_argument(
+    "--timeout",
+    dest="TIMEOUT",
+    default=10,
+    type=int,
+    help="Timeout (seconds) before test ends",
+)
+parser.add_argument(
+    "--list",
+    dest="LIST",
+    action="store_true",
+    help="list audio devices by number and exit",
+)
 
 args, _ = parser.parse_known_args()
 
 if args.LIST:
     p = pyaudio.PyAudio()
-    for dev in range(0,p.get_device_count()):
+    for dev in range(p.get_device_count()):
         print("audiodev: ", dev, p.get_device_info_by_index(dev)["name"])
-    quit()
+    sys.exit()
 
 
-
-class Test():
+class Test:
     def __init__(self):
         self.N_BURSTS = args.N_BURSTS
         self.N_FRAMES_PER_BURST = args.N_FRAMES_PER_BURST
@@ -51,12 +65,16 @@ class Test():
         self.TIMEOUT = args.TIMEOUT
 
         # AUDIO PARAMETERS
-        self.AUDIO_FRAMES_PER_BUFFER = 2400*2  # <- consider increasing if you get nread_exceptions > 0
+        self.AUDIO_FRAMES_PER_BUFFER = (
+            2400 * 2
+        )  # <- consider increasing if you get nread_exceptions > 0
         self.MODEM_SAMPLE_RATE = codec2.api.FREEDV_FS_8000
         self.AUDIO_SAMPLE_RATE_RX = 48000
 
         # make sure our resampler will work
-        assert (self.AUDIO_SAMPLE_RATE_RX / self.MODEM_SAMPLE_RATE) == codec2.api.FDMDV_OS_48
+        assert (
+            self.AUDIO_SAMPLE_RATE_RX / self.MODEM_SAMPLE_RATE
+        ) == codec2.api.FDMDV_OS_48
 
         # check if we want to use an audio device then do an pyaudio init
         if self.AUDIO_INPUT_DEVICE != -1:
@@ -64,37 +82,43 @@ class Test():
             # auto search for loopback devices
             if self.AUDIO_INPUT_DEVICE == -2:
                 loopback_list = []
-                for dev in range(0,self.p.get_device_count()):
-                    if 'Loopback: PCM' in self.p.get_device_info_by_index(dev)["name"]:
+                for dev in range(self.p.get_device_count()):
+                    if "Loopback: PCM" in self.p.get_device_info_by_index(dev)["name"]:
                         loopback_list.append(dev)
                 if len(loopback_list) >= 2:
-                    self.AUDIO_INPUT_DEVICE = loopback_list[0] #0  = RX   1 = TX
+                    self.AUDIO_INPUT_DEVICE = loopback_list[0]  # 0  = RX   1 = TX
                     print(f"loopback_list rx: {loopback_list}", file=sys.stderr)
                 else:
-                    quit()
+                    sys.exit()
 
-            print(f"AUDIO INPUT DEVICE: {self.AUDIO_INPUT_DEVICE} DEVICE: {self.p.get_device_info_by_index(self.AUDIO_INPUT_DEVICE)['name']}  \
-                    AUDIO SAMPLE RATE: {self.AUDIO_SAMPLE_RATE_RX}", file=sys.stderr)
+            print(
+                f"AUDIO INPUT DEVICE: {self.AUDIO_INPUT_DEVICE} DEVICE: {self.p.get_device_info_by_index(self.AUDIO_INPUT_DEVICE)['name']}  \
+                    AUDIO SAMPLE RATE: {self.AUDIO_SAMPLE_RATE_RX}",
+                file=sys.stderr,
+            )
 
-            self.stream_rx = self.p.open(format=pyaudio.paInt16,
-                                    channels=1,
-                                    rate=self.AUDIO_SAMPLE_RATE_RX,
-                                    frames_per_buffer=self.AUDIO_FRAMES_PER_BUFFER,
-                                    input=True,
-                                    output=False,
-                                    input_device_index=self.AUDIO_INPUT_DEVICE,
-                                    stream_callback=self.callback
-                                    )
+            self.stream_rx = self.p.open(
+                format=pyaudio.paInt16,
+                channels=1,
+                rate=self.AUDIO_SAMPLE_RATE_RX,
+                frames_per_buffer=self.AUDIO_FRAMES_PER_BUFFER,
+                input=True,
+                output=False,
+                input_device_index=self.AUDIO_INPUT_DEVICE,
+                stream_callback=self.callback,
+            )
 
         # open codec2 instance
-        self.freedv = cast(codec2.api.freedv_open(self.MODE), c_void_p)
+        self.freedv = ctypes.cast(codec2.api.freedv_open(self.MODE), ctypes.c_void_p)
 
         # get number of bytes per frame for mode
-        self.bytes_per_frame = int(codec2.api.freedv_get_bits_per_modem_frame(self.freedv)/8)
+        self.bytes_per_frame = int(
+            codec2.api.freedv_get_bits_per_modem_frame(self.freedv) / 8
+        )
 
-        self.bytes_out = create_string_buffer(self.bytes_per_frame * 2)
+        self.bytes_out = ctypes.create_string_buffer(self.bytes_per_frame * 2)
 
-        codec2.api.freedv_set_frames_per_burst(self.freedv,self.N_FRAMES_PER_BURST)
+        codec2.api.freedv_set_frames_per_burst(self.freedv, self.N_FRAMES_PER_BURST)
 
         self.total_n_bytes = 0
         self.rx_total_frames = 0
@@ -104,13 +128,13 @@ class Test():
         self.nread_exceptions = 0
         self.timeout = time.time() + self.TIMEOUT
         self.receive = True
-        self.audio_buffer = codec2.audio_buffer(self.AUDIO_FRAMES_PER_BUFFER*2)
+        self.audio_buffer = codec2.audio_buffer(self.AUDIO_FRAMES_PER_BUFFER * 2)
         self.resampler = codec2.resampler()
 
         # Copy received 48 kHz to a file.  Listen to this file with:
         #   aplay -r 48000 -f S16_LE rx48_callback.raw
         # Corruption of this file is a good way to detect audio card issues
-        self.frx = open("rx48_callback.raw", mode='wb')
+        self.frx = open("rx48_callback.raw", mode="wb")
 
     def callback(self, data_in48k, frame_count, time_info, status):
 
@@ -128,15 +152,16 @@ class Test():
         except Exception as e:
             print(f"pyAudio error: {e}", file=sys.stderr)
 
-
         while self.receive and time.time() < self.timeout:
-            #time.sleep(1)
+            # time.sleep(1)
             # when we have enough samples call FreeDV Rx
             nin = codec2.api.freedv_nin(self.freedv)
             while self.audio_buffer.nbuffer >= nin:
 
                 # demodulate audio
-                nbytes = codec2.api.freedv_rawdatarx(self.freedv, self.bytes_out, self.audio_buffer.buffer.ctypes)
+                nbytes = codec2.api.freedv_rawdatarx(
+                    self.freedv, self.bytes_out, self.audio_buffer.buffer.ctypes
+                )
                 self.audio_buffer.pop(nin)
 
                 # call me on every loop!
@@ -146,9 +171,12 @@ class Test():
                 if rx_status & codec2.api.FREEDV_RX_BIT_ERRORS:
                     self.rx_errors = self.rx_errors + 1
                 if self.DEBUGGING_MODE:
-                  rx_status = codec2.api.rx_sync_flags_to_text[rx_status]
-                  print("nin: %5d rx_status: %4s naudio_buffer: %4d" % \
-                        (nin,rx_status,self.audio_buffer.nbuffer), file=sys.stderr)
+                    rx_status = codec2.api.rx_sync_flags_to_text[rx_status]
+                    print(
+                        "nin: %5d rx_status: %4s naudio_buffer: %4d"
+                        % (nin, rx_status, self.audio_buffer.nbuffer),
+                        file=sys.stderr,
+                    )
 
                 if nbytes:
                     self.total_n_bytes = self.total_n_bytes + nbytes
@@ -167,9 +195,15 @@ class Test():
             print("TIMEOUT REACHED")
 
         if self.nread_exceptions:
-            print("nread_exceptions %d - receive audio lost! Consider increasing Pyaudio frames_per_buffer..." %  \
-                  self.nread_exceptions, file=sys.stderr)
-        print(f"RECEIVED BURSTS: {self.rx_bursts} RECEIVED FRAMES: {self.rx_total_frames} RX_ERRORS: {self.rx_errors}", file=sys.stderr)
+            print(
+                "nread_exceptions %d - receive audio lost! Consider increasing Pyaudio frames_per_buffer..."
+                % self.nread_exceptions,
+                file=sys.stderr,
+            )
+        print(
+            f"RECEIVED BURSTS: {self.rx_bursts} RECEIVED FRAMES: {self.rx_total_frames} RX_ERRORS: {self.rx_errors}",
+            file=sys.stderr,
+        )
         self.frx.close()
 
         # cloese pyaudio instance
--- test/test_tnc.py
+++ test/test_tnc.py
@@ -5,22 +5,27 @@ import argparse
 import sys
 import time
 
-sys.path.insert(0,'..')
-sys.path.insert(0,'../tnc')
+sys.path.insert(0, "..")
+sys.path.insert(0, "../tnc")
 import data_handler
 import helpers
 import modem
 import static
 
-parser = argparse.ArgumentParser(description='ARQ TEST')
-parser.add_argument('--ISS', dest="ISS", action="store_true")
-parser.add_argument('--IRS', dest="IRS", action="store_true")
+parser = argparse.ArgumentParser(description="ARQ TEST")
+parser.add_argument("--ISS", dest="ISS", action="store_true")
+parser.add_argument("--IRS", dest="IRS", action="store_true")
 
-parser.add_argument('--CQ', dest="CQ", action="store_true")
-parser.add_argument('--PING', dest="PING", action="store_true")
-parser.add_argument('--RAW', dest="RAW", action="store_true")
+parser.add_argument("--CQ", dest="CQ", action="store_true")
+parser.add_argument("--PING", dest="PING", action="store_true")
+parser.add_argument("--RAW", dest="RAW", action="store_true")
 
 args, _ = parser.parse_known_args()
+
+if not (args.CQ or args.PING or args.RAW or args.ISS or args.IRS):
+    print("No arguments provided.")
+    print("Exiting.")
+    sys.exit()
 
 ISS = args.ISS
 IRS = args.IRS
@@ -32,21 +37,21 @@ RAW = args.RAW
 # ------ initialize TNC with test mode
 data_handler.TESTMODE = True
 modem.TESTMODE = True
-static.HAMLIB_RADIOCONTROL = 'disabled'
+static.HAMLIB_RADIOCONTROL = "disabled"
 
 if ISS:
     modem.RXCHANNEL = "hfchannel1"
     modem.TXCHANNEL = "hfchannel2"
 
-    mycallsign = bytes('DJ2LS-2', 'utf-8')
+    mycallsign = bytes("DJ2LS-2", "utf-8")
     mycallsign = helpers.callsign_to_bytes(mycallsign)
     static.MYCALLSIGN = helpers.bytes_to_callsign(mycallsign)
     static.MYCALLSIGN_CRC = helpers.get_crc_24(static.MYCALLSIGN)
-    static.MYGRID = bytes('AA12aa', 'utf-8')
+    static.MYGRID = bytes("AA12aa", "utf-8")
     static.SSID_LIST = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     # set dx dxcallsign
-    dxcallsign = b'DN2LS-0'
+    dxcallsign = b"DN2LS-0"
     dxcallsign = helpers.callsign_to_bytes(dxcallsign)
     dxcallsign = helpers.bytes_to_callsign(dxcallsign)
     static.DXCALLSIGN = dxcallsign
@@ -56,13 +61,13 @@ if IRS:
     modem.RXCHANNEL = "hfchannel2"
     modem.TXCHANNEL = "hfchannel1"
 
-    mycallsign = bytes('DN2LS-2', 'utf-8')
+    mycallsign = bytes("DN2LS-2", "utf-8")
     mycallsign = helpers.callsign_to_bytes(mycallsign)
     static.MYCALLSIGN = helpers.bytes_to_callsign(mycallsign)
     static.MYCALLSIGN_CRC = helpers.get_crc_24(static.MYCALLSIGN)
-    static.MYGRID = bytes('AA12aa', 'utf-8')
+    static.MYGRID = bytes("AA12aa", "utf-8")
     static.RESPOND_TO_CQ = True
-    static.SSID_LIST = [0,1,2,3,4,5,6,7,8,9]
+    static.SSID_LIST = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # start data handler
 data_handler.DATA()
@@ -75,7 +80,7 @@ if RAW:
     bytes_out = b'{"dt":"f","fn":"zeit.txt","ft":"text\\/plain","d":"data:text\\/plain;base64,MyBtb2Rlcywgb2huZSBjbGFzcwowLjAwMDk2OTQ4MTE4MDk5MTg0MTcKCjIgbW9kZXMsIG9obmUgY2xhc3MKMC4wMDA5NjY1NDUxODkxMjI1Mzk0CgoxIG1vZGUsIG9obmUgY2xhc3MKMC4wMDA5NjY5NzY1NTU4Nzc4MjA5CgMyBtb2Rlcywgb2huZSBjbGFzcwowLjAwMDk2OTQ4MTE4MDk5MTg0MTcKCjIgbW9kZXMsIG9obmUgY2xhc3MKMC4wMDA5NjY1NDUxODkxMjI1Mzk0CgoxIG1vZGUsIG9obmUgY2xhc3MKMC4wMDA5NjY5NzY1NTU4Nzc4MjA5Cg=MyBtb2Rlcywgb2huZSBjbGFzcwowLjAwMDk2OTQ4MTE4MDk5MTg0MTcKCjIgbW9kZXMsIG9obmUgY2xhc3MKMC4wMDA5NjY1NDUxODkxMjI1Mzk0CgoxIG1vZGUsIG9obmUgY2xhc3MKMC4wMDA5NjY5NzY1NTU4Nzc4MjA5CgMyBtb2Rlcywgb2huZSBjbGFzcwowLjAwMDk2OTQ4MTE4MDk5MTg0MTcKCjIgbW9kZXMsIG9obmUgY2xhc3MKMC4wMDA5NjY1NDUxODkxMjI1Mzk0CgoxIG1vZGUsIG9obmUgY2xhc3MKMC4wMDA5NjY5NzY1NTU4Nzc4MjA5CgMyBtb2Rlcywgb2huZSBjbGFzcwowLjAwMDk2OTQ4MTE4MDk5MTg0MTcKCjIgbW9kZXMsIG9obmUgY2xhc3MKMC4wMDA5NjY1NDUxODkxMjI1Mzk0CgoxIG1vZGUsIG9obmUgY2xhc3MKMC4wMDA5NjY5NzY1NTU4Nzc4MjA5Cg=","crc":"123123123"}'
 
     # add command to data qeue
-    '''
+    """
     data[0] == 'ARQ_RAW':
         # [0] ARQ_RAW
         # [1] DATA_OUT bytes
@@ -83,18 +88,18 @@ if RAW:
         # [3] N_FRAMES_PER_BURST int
         # [4] self.transmission_uuid str
         # [5] mycallsign with ssid
-    '''
-    #data_handler.DATA_QUEUE_TRANSMIT.put(['ARQ_RAW', bytes_out, 255, n_frames_per_burst, '123', b'DJ2LS-0'])
+    """
+    # data_handler.DATA_QUEUE_TRANSMIT.put(['ARQ_RAW', bytes_out, 255, n_frames_per_burst, '123', b'DJ2LS-0'])
 
 # transmit CQ
 if CQ:
     for _ in range(4):
-        data_handler.DATA_QUEUE_TRANSMIT.put(['CQ'])
+        data_handler.DATA_QUEUE_TRANSMIT.put(["CQ"])
 
 # transmit PING
 if PING:
     for _ in range(4):
-        data_handler.DATA_QUEUE_TRANSMIT.put(['PING', b'DN2LS-2'])
+        data_handler.DATA_QUEUE_TRANSMIT.put(["PING", b"DN2LS-2"])
 
 while 1:
     time.sleep(0.01)
