d258fa74b3f8ecf3e3675f3151c52a123ae0ba88
Paul Kronenwetter <kronenpj@gmail.com>
2022-06-06 18:08:32 -0400

    Update checks for successful transmission.

--- test/util_data_channel_2.py
+++ test/util_data_channel_2.py
@@ -120,7 +120,7 @@ def t_highsnr_arq_short_station2(
     timeout = time.time() + 25
     # Compare with the string conversion instead of repeatedly dumping
     # the queue to an object for comparisons.
-    while '""arq":"received"' not in str(sock.SOCKET_QUEUE.queue) or static.ARQ_STATE:
+    while '"arq":"received"' not in str(sock.SOCKET_QUEUE.queue) or static.ARQ_STATE:
         if time.time() > timeout:
             log.warning("station2 TIMEOUT", first=True)
             break
