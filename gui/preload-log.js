e55623e0aedcda1e991832c3d43a7687709511a5
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2022-11-09 16:46:25 +0100

    added build folder for getting splash screen working - buildResources will be ignored on import

--- gui/preload-log.js
+++ gui/preload-log.js
@@ -8,6 +8,28 @@ var configPath = path.join(configFolder, 'config.json')
 const config = require(configPath);
 
 
+var _getAllFilesFromFolder = function(dir) {
+
+    var filesystem = require("fs");
+    var results = [];
+
+    filesystem.readdirSync(dir).forEach(function(file) {
+
+        file = dir+'/'+file;
+        var stat = filesystem.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(_getAllFilesFromFolder(file))
+        } else results.push(file);
+
+    });
+
+    return results;
+
+};
+
+console.log(_getAllFilesFromFolder(__dirname));
+
 
 // WINDOW LISTENER
 window.addEventListener('DOMContentLoaded', () => {
