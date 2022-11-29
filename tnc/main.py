1c00fcd13b3b984b3b5e2916eb85299dc28d890d
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2022-11-29 07:45:17 +0100

    fixed ssid list parameter

--- tnc/main.py
+++ tnc/main.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
         nargs="*",
         default=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
         help="SSID list we are responding to",
-        type=str,
+        type=int,
     )
     PARSER.add_argument(
         "--mygrid",
