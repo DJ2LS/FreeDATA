2e12b2ed929c4268d1bc742b3898eac7590b903f
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2022-12-11 12:23:37 +0100

    increase amount of NACK frames for failed data frame

--- tnc/data_handler.py
+++ tnc/data_handler.py
@@ -528,7 +528,7 @@ class DATA:
         # TRANSMIT NACK FRAME FOR BURST
         # TODO: Do we have to send ident frame?
         # self.enqueue_frame_for_tx([ack_frame, self.send_ident_frame(False)], c2_mode=FREEDV_MODE.sig1.value, copies=3, repeat_delay=0)
-        self.enqueue_frame_for_tx([nack_frame], c2_mode=FREEDV_MODE.sig1.value, copies=1, repeat_delay=0)
+        self.enqueue_frame_for_tx([nack_frame], c2_mode=FREEDV_MODE.sig1.value, copies=6, repeat_delay=0)
     def send_burst_nack_frame_watchdog(self, snr: bytes) -> None:
         """Build and send NACK frame for watchdog timeout"""
 
