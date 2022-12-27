b637b917bc2677ec9405bb4ce842c9ac4b397c2d
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2022-12-27 22:57:54 +0100

    first attempt with message checksum

--- gui/sock.js
+++ gui/sock.js
@@ -613,3 +613,11 @@ ipcRenderer.on('action-update-tnc-ip', (event, arg) => {
     connectTNC();
 
 });
+
+
+
+// https://stackoverflow.com/a/50579690
+// crc32 calculation
+//console.log(crc32('abc'));
+//console.log(crc32('abc').toString(16).toUpperCase()); // hex
+var crc32=function(r){for(var a,o=[],c=0;c<256;c++){a=c;for(var f=0;f<8;f++)a=1&a?3988292384^a>>>1:a>>>1;o[c]=a}for(var n=-1,t=0;t<r.length;t++)n=n>>>8^o[255&(n^r.charCodeAt(t))];return(-1^n)>>>0};
