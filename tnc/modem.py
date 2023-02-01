021d6fd79a4d5b9c5b837bcc8bb8bc0e27b7d5f2
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2023-02-01 15:06:26 +0100

    increase audio level if too low for ALC

--- tnc/modem.py
+++ tnc/modem.py
@@ -544,12 +544,14 @@ class RF:
         # Re-sample back up to 48k (resampler works on np.int16)
         x = np.frombuffer(txbuffer, dtype=np.int16)
         if static.AUDIO_AUTO_TUNE:
-            if 0.001 < static.HAMLIB_ALC <= 0.8:
+            if static.HAMLIB_ALC == 0.0 and static.TX_AUDIO_LEVEL <= 10:
+                static.TX_AUDIO_LEVEL = 30
+            elif 0.0 < static.HAMLIB_ALC <= 0.8:
                 print("0.001 > static.HAMLIB_ALC <= 0.8")
                 static.TX_AUDIO_LEVEL = static.TX_AUDIO_LEVEL + 20
                 self.log.debug("[MDM] AUDIO TUNE", level=str(static.TX_AUDIO_LEVEL), alc=str(static.HAMLIB_ALC))
             elif 0.8 < static.HAMLIB_ALC < 0.99:
-                print("0.001 > static.HAMLIB_ALC <= 0.8")
+                print("0.8 > static.HAMLIB_ALC <= 0.99")
                 static.TX_AUDIO_LEVEL = static.TX_AUDIO_LEVEL + 2
                 self.log.debug("[MDM] AUDIO TUNE", level=str(static.TX_AUDIO_LEVEL), alc=str(static.HAMLIB_ALC))
             elif 1.0 < static.HAMLIB_ALC:
