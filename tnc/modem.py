eaff3210ed16f557fcd76d0316122f279822f6a7
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2023-02-09 21:48:55 +0100

    adjusted alc tuning

--- tnc/modem.py
+++ tnc/modem.py
@@ -537,17 +537,18 @@ class RF:
         x = np.frombuffer(txbuffer, dtype=np.int16)
         if static.AUDIO_AUTO_TUNE:
             if static.HAMLIB_ALC == 0.0:
-                static.TX_AUDIO_LEVEL = static.TX_AUDIO_LEVEL + 30
-            elif 0.0 < static.HAMLIB_ALC <= 0.8:
-                print("0.001 > static.HAMLIB_ALC <= 0.8")
                 static.TX_AUDIO_LEVEL = static.TX_AUDIO_LEVEL + 20
-                self.log.debug("[MDM] AUDIO TUNE", audio_level=str(static.TX_AUDIO_LEVEL), alc_level=str(static.HAMLIB_ALC))
-            elif 0.8 < static.HAMLIB_ALC < 0.99:
-                print("0.8 > static.HAMLIB_ALC <= 0.99")
+            elif 0.0 < static.HAMLIB_ALC <= 0.1:
+                print("0.0 < static.HAMLIB_ALC <= 0.1")
                 static.TX_AUDIO_LEVEL = static.TX_AUDIO_LEVEL + 2
                 self.log.debug("[MDM] AUDIO TUNE", audio_level=str(static.TX_AUDIO_LEVEL), alc_level=str(static.HAMLIB_ALC))
-            elif 1.0 <= static.HAMLIB_ALC:
-                static.TX_AUDIO_LEVEL = static.TX_AUDIO_LEVEL - 2
+            elif 0.1 < static.HAMLIB_ALC < 0.2:
+                print("0.1 < static.HAMLIB_ALC < 0.2")
+                static.TX_AUDIO_LEVEL = static.TX_AUDIO_LEVEL
+                self.log.debug("[MDM] AUDIO TUNE", audio_level=str(static.TX_AUDIO_LEVEL), alc_level=str(static.HAMLIB_ALC))
+            elif 0.2 < static.HAMLIB_ALC < 0.99:
+                print("0.2 < static.HAMLIB_ALC < 0.99")
+                static.TX_AUDIO_LEVEL = static.TX_AUDIO_LEVEL - 20
                 self.log.debug("[MDM] AUDIO TUNE", audio_level=str(static.TX_AUDIO_LEVEL), alc_level=str(static.HAMLIB_ALC))
             else:
                 self.log.debug("[MDM] AUDIO TUNE", audio_level=str(static.TX_AUDIO_LEVEL), alc_level=str(static.HAMLIB_ALC))
