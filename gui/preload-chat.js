fffb1145fe2b8f35a2b1ef8bff52ac9c3e335fe8
Mashintime <Mashintime@users.noreply.github.com>
2023-03-04 18:49:59 +0000

    Prettified Code!

--- gui/preload-chat.js
+++ gui/preload-chat.js
@@ -1875,7 +1875,12 @@ async function updateAllChat(clear) {
 function getSetUserInformation(selected_callsign) {
   //Get user information
 
-  if (selected_callsign == "" || selected_callsign == null || typeof selected_callsign == "undefined") return;
+  if (
+    selected_callsign == "" ||
+    selected_callsign == null ||
+    typeof selected_callsign == "undefined"
+  )
+    return;
 
   document.getElementById("dx_user_info_callsign").innerHTML =
     selected_callsign;
