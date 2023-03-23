05cf80a89d6128c38047ec85d0706f48167133a5
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2023-03-23 10:43:58 +0100

    version update

--- tnc/static.py
+++ tnc/static.py
@@ -11,7 +11,7 @@ Not nice, suggestions are appreciated :-)
 import subprocess
 from enum import Enum
 
-VERSION = "0.8.0-alpha.3"
+VERSION = "0.8.0-alpha.4"
 
 ENABLE_EXPLORER = False
 ENABLE_STATS = False
