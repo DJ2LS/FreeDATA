a8e4e1ee2f74c0cad239399d7d6d189dcbb45dbc
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2023-03-29 08:45:23 +0200

    adjusted ptt trigger for tci

--- tnc/modem.py
+++ tnc/modem.py
@@ -335,8 +335,8 @@ class RF:
         while True:
             threading.Event().wait(0.01)
 
-            if len(self.modoutqustatic.PTT_STATEeue) > 0 and not self.mod_out_locked:
-                 = self.radio.set_ptt(True)
+            if len(self.modoutqueue) > 0 and not self.mod_out_locked:
+                static.PTT_STATE = self.radio.set_ptt(True)
                 jsondata = {"ptt": "True"}
                 data_out = json.dumps(jsondata)
                 sock.SOCKET_QUEUE.put(data_out)
