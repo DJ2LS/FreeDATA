9e30dd9ef9f06226fc6654c17ff3e80ddf0dbbe5
DJ2LS <dj2ls@proton.me>
2023-05-04 18:57:15 +0200

    version update for hotfixes

--- tnc/static.py
+++ tnc/static.py
@@ -127,7 +127,7 @@ class TCIParam:
 
 @dataclass 
 class TNC:
-    version = "0.9.0-alpha.1"
+    version = "0.9.0-alpha.2"
     host: str = "0.0.0.0"
     port: int = 3000
     SOCKET_TIMEOUT: int = 1  # seconds
