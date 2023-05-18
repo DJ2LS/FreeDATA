c0533445b0413b83264e0ba52bd03375f8bfb351
Mashintime <awh@brainless.us>
2023-05-18 16:22:00 -0400

    Ver .4

--- tnc/static.py
+++ tnc/static.py
@@ -127,7 +127,7 @@ class TCIParam:
 
 @dataclass 
 class TNC:
-    version = "0.9.2-alpha.3"
+    version = "0.9.2-alpha.4"
     host: str = "0.0.0.0"
     port: int = 3000
     SOCKET_TIMEOUT: int = 1  # seconds
