82c6e6462eb75c3fcfd41613d062dbddd17aa1d4
DJ2LS <dj2ls@proton.me>
2023-05-29 18:55:37 +0200

    updated version

--- tnc/static.py
+++ tnc/static.py
@@ -129,7 +129,7 @@ class TCIParam:
 
 @dataclass 
 class TNC:
-    version = "0.10.0-alpha.1-mesh-exp6"
+    version = "0.10.0-alpha.1-mesh-exp7"
     host: str = "0.0.0.0"
     port: int = 3000
     SOCKET_TIMEOUT: int = 1  # seconds
