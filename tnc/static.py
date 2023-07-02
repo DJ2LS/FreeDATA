23ed5be2d88761ee229d460ec281e0a35061ef44
Mashintime <awh@brainless.us>
2023-07-02 09:47:14 -0400

    Bump version

--- tnc/static.py
+++ tnc/static.py
@@ -125,7 +125,7 @@ class TCIParam:
 
 @dataclass 
 class TNC:
-    version = "0.9.3-alpha.2"
+    version = "0.9.4-alpha.1"
     host: str = "0.0.0.0"
     port: int = 3000
     SOCKET_TIMEOUT: int = 1  # seconds
