7f411d6365411fc7bf077c112e05a8ad9a2f0700
DJ2LS <dj2ls@proton.me>
2023-10-22 21:42:29 +0200

    test with new notarization for macos

---
+++ gui/build/notarize_macos.sh
@@ -0,0 +1,19 @@
+const { notarize } = require('@electron/notarize');
+
+async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: 'app.freedata',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+  });
+}
+
+exports.default = notarizing;
