9cb5134737816c8d27f888e957603d17ea938ef0
DJ2LS <dj2ls@proton.me>
2023-10-23 13:36:10 +0200

    version update

--- modem/static.py
+++ modem/static.py
@@ -134,7 +134,7 @@ class TCIParam:
 
 @dataclass 
 class Modem:
-    version = "0.11.0-alpha.5"
+    version = "0.11.0-alpha.6"
     host: str = "0.0.0.0"
     port: int = 3000
     SOCKET_TIMEOUT: int = 1  # seconds
