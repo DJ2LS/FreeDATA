0f6ee3038926881ba4d466b322b30073bdf30230
Pedro Monteiro <ptsmonteiro@gmail.com>
2023-11-01 15:36:24 +0100

    Move station settings to the settings area

--- gui/src/components/settings.vue
+++ gui/src/components/settings.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import settings_station from "./settings_station.vue";
 import settings_gui from "./settings_gui.vue";
 import settings_chat from "./settings_chat.vue";
 import settings_hamlib from "./settings_hamlib.vue";
@@ -28,6 +29,20 @@ import settings_exp from "./settings_exp.vue";
             <li class="nav-item" role="presentation">
               <button
                 class="nav-link active"
+                id="station-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#station"
+                type="button"
+                role="tab"
+                aria-controls="home"
+                aria-selected="true"
+              >
+                Station
+              </button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button
+                class="nav-link"
                 id="gui-tab"
                 data-bs-toggle="tab"
                 data-bs-target="#gui"
@@ -113,10 +128,22 @@ import settings_exp from "./settings_exp.vue";
         </div>
         <div class="card-body">
           <!-- SETTINGS Nav Tab panes -->
+
+          <!-- Station tab contents-->
           <div class="tab-content">
-            <!-- GUI tab contents-->
             <div
               class="tab-pane active"
+              id="station"
+              role="tabpanel"
+              aria-labelledby="station-tab"
+              tabindex="0"
+            >
+              <settings_station />
+            </div>
+
+            <!-- GUI tab contents-->
+            <div
+              class="tab-pane"
               id="gui"
               role="tabpanel"
               aria-labelledby="gui-tab"
---
+++ gui/src/components/settings_station.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { saveSettingsToFile } from "../js/settingsHandler";
+
+import { setActivePinia } from "pinia";
+import pinia from "../store/index";
+setActivePinia(pinia);
+
+import { useSettingsStore } from "../store/settingsStore.js";
+const settings = useSettingsStore(pinia);
+
+function saveSettings() {
+  saveSettingsToFile();
+}
+</script>
+<template>
+
+  <!-- station callsign -->
+  <div class="input-group input-group-sm mb-1">
+      <span class="input-group-text" style="width: 180px">Your station callsign</span>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Enter your callsign and save it"
+        id="myCall"
+        aria-label="Station Callsign"
+        aria-describedby="basic-addon1"
+        @change="saveSettings"
+        v-model="settings.mycall"
+      />
+  </div>
+
+  <!-- station ssid -->
+  <div class="input-group input-group-sm mb-1">
+    <span class="input-group-text" style="width: 180px">Call SSID</span>
+    <select
+      class="form-select form-select-sm w-50"
+      id="myCallSSID"
+      @change="saveSettings"
+      v-model="settings.myssid"
+    >
+      <option selected value="0">0</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+      <option value="9">9</option>
+      <option value="10">10</option>
+      <option value="11">11</option>
+      <option value="12">12</option>
+      <option value="13">13</option>
+      <option value="14">14</option>
+      <option value="15">15</option>
+    </select>
+  </div>
+
+  <!-- station grid locator -->
+  <div class="input-group input-group-sm mb-1">
+      <span class="input-group-text" style="width: 180px">Grid Locator</span>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Your grid locator"
+        id="myGrid"
+        maxlength="6"
+        aria-label="Station Grid Locator"
+        aria-describedby="basic-addon1"
+        @change="saveSettings"
+        v-model="settings.mygrid"
+      />
+  </div>
+
+</template>
