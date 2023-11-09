119eb4ed883a15d69970d9d8e58469e359ea2efd
codefactor-io <support@codefactor.io>
2023-11-09 12:52:47 +0000

    [CodeFactor] Apply fixes

--- gui/src/js/event_sock.js
+++ gui/src/js/event_sock.js
@@ -3,40 +3,38 @@ let retries = 0;
 let maxRetries = 15;
 
 function connect() {
+  socket = new WebSocket("ws://localhost:5000/events");
 
-    socket = new WebSocket("ws://localhost:5000/events");
+  // handle opening
+  socket.addEventListener("open", function (event) {
+    console.log("Connected to the WebSocket server");
+    retries = 0; // Reset the retries back to 0 since the connection was successful
+  });
 
-    // handle opening
-    socket.addEventListener('open', function (event) {
-        console.log('Connected to the WebSocket server');
-        retries = 0; // Reset the retries back to 0 since the connection was successful
-    });
+  // handle data
+  socket.addEventListener("message", function (event) {
+    console.log("Message from server:", event.data);
+    console.log("Message from server:", event);
+  });
 
-    // handle data
-    socket.addEventListener('message', function (event) {
-        console.log('Message from server:', event.data);
-        console.log('Message from server:', event);
+  // handle errors
+  socket.addEventListener("error", function (event) {
+    console.error("WebSocket error:", event);
+  });
 
-    });
+  // handle closing and reconnect
+  socket.addEventListener("close", function (event) {
+    console.log("WebSocket connection closed:", event.code);
 
-    // handle errors
-    socket.addEventListener('error', function (event) {
-        console.error('WebSocket error:', event);
-    });
-
-    // handle closing and reconnect
-    socket.addEventListener('close', function (event) {
-        console.log('WebSocket connection closed:', event.code);
-
-        // Reconnect handler
-        if (!event.wasClean && retries < maxRetries) {
-            setTimeout(() => {
-                console.log("Reconnecting to websocket. Attempt: " + retries)
-                connect();
-            }, 1000);
-            retries++;
-        }
-    });
+    // Reconnect handler
+    if (!event.wasClean && retries < maxRetries) {
+      setTimeout(() => {
+        console.log("Reconnecting to websocket. Attempt: " + retries);
+        connect();
+      }, 1000);
+      retries++;
+    }
+  });
 }
 
 // Initial connection attempt
