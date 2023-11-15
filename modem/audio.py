924871859ff6207059d2fbb3d5f3eadb572b971b
Pedro <ptsmonteiro@gmail.com>
2023-11-15 21:14:56 +0100

    Fix identification of audio devices

--- modem/audio.py
+++ modem/audio.py
@@ -49,7 +49,7 @@ def get_audio_devices():
 
 
 def device_crc(device) -> str:
-    crc_hwid = crc_algorithm(bytes(f"{device}", encoding="utf-8"))
+    crc_hwid = crc_algorithm(bytes(f"{device['name']}.{device['hostapi']}", encoding="utf-8"))
     crc_hwid = crc_hwid.to_bytes(2, byteorder="big")
     crc_hwid = crc_hwid.hex()
     return crc_hwid
