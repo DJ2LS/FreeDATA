e3e486b5fceff03490d492a78fd9dc81544b5b35
Pedro <ptsmonteiro@gmail.com>
2023-11-26 12:53:28 +0100

    Fix PingCommand

--- modem/command_ping.py
+++ modem/command_ping.py
@@ -2,9 +2,9 @@ from command import TxCommand
 
 class PingCommand(TxCommand):
 
-    def setParamsFromApi(self, apiParams):
+    def set_params_from_api(self, apiParams):
         self.dxcall = apiParams['dxcall']
-        return super().setParamsFromApi()
+        return super().set_params_from_api(apiParams)
 
     def build_frame(self):
         return self.frame_factory.build_ping(self.dxcall)
