bb0fc79b4f2505c6644c8f872a7cdceb98bf53a6
Pedro <ptsmonteiro@gmail.com>
2023-12-06 11:51:10 +0100

    Improve ARQ logging

--- tests/test_arq_session.py
+++ tests/test_arq_session.py
@@ -51,9 +51,9 @@ class TestARQSession(unittest.TestCase):
             transmission_item = modem_transmit_queue.get()
             frame_bytes = bytes(transmission_item['frame'])
             if random.randint(0, 100) < self.loss_probability:
-                self.logger.info("Frame lost...")
+                self.logger.info("[TestChannel] Frame lost...")
                 continue
-            self.logger.info("Redirecting frame")
+            self.logger.info("[TestChannel] Redirecting frame")
             frame_dispatcher.new_process_data(frame_bytes, None, len(frame_bytes), 0, 0)
 
     def establishChannels(self):
