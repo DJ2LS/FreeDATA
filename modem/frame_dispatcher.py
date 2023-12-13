cefd1b3efcf317acb60255c1307fb6890b60648b
DJ2LS <dj2ls@proton.me>
2023-12-13 23:53:39 +0100

    WIP ARQ - cleanup

--- modem/frame_dispatcher.py
+++ modem/frame_dispatcher.py
@@ -9,15 +9,6 @@ from modem_frametypes import FRAME_TYPE as FR_TYPE
 import event_manager
 from data_frame_factory import DataFrameFactory
 
-#from deprecated_data_handler_broadcasts import BROADCAST
-#from deprecated_data_handler_data_broadcasts import DATABROADCAST
-#from deprecated_data_handler_ping import PING
-
-from deprecated_protocol_arq_session_iss import ISS
-from deprecated_protocol_arq_session_irs import IRS
-from deprecated_protocol_arq_session import ARQ
-from deprecated_protocol_arq_connection import SESSION
-
 from frame_handler import FrameHandler
 from frame_handler_ping import PingFrameHandler
 from frame_handler_cq import CQFrameHandler
@@ -59,7 +50,6 @@ class DISPATCHER():
         self.states = states
 
         self._initialize_handlers(config, event_queue, states)
-        #self._initialize_dispatchers()
 
         self.data_queue_received = data_q_rx
         self.modem_transmit_queue = modem_tx_q
@@ -70,16 +60,6 @@ class DISPATCHER():
         """Initializes various data handlers."""
 
         self.frame_factory = DataFrameFactory(config)
-
-        #self.broadcasts = BROADCAST(config, event_queue, states)
-        #self.data_broadcasts = DATABROADCAST(config, event_queue, states)
-        #self.ping = PING(config, event_queue, states)
-
-        self.arq = ARQ(config, event_queue, states)
-        self.arq_irs = IRS(config, event_queue, states)
-        self.arq_iss = ISS(config, event_queue, states)
-        self.arq_session = SESSION(config, event_queue, states)
-
         self.event_manager = event_manager.EventManager([event_queue])
 
     def start(self):
@@ -118,31 +98,11 @@ class DISPATCHER():
 
         handler.handle(deconstructed_frame, snr, frequency_offset, freedv, bytes_per_frame)
 
-
-
     def get_id_from_frame(self, data):
         if data[:1] == FR_TYPE.ARQ_SESSION_OPEN:
             return data[13:14]
         return None
 
-    def initialize_arq_instance(self):
-        self.arq = ARQ(self.config, self.event_queue, self.states)
-        self.arq_irs = IRS(self.config, self.event_queue, self.states)
-        self.arq_iss = ISS(self.config, self.event_queue, self.states)
-        self.arq_session = SESSION(self.config, self.event_queue, self.states)
-
-        return {
-            'arq': self.arq,
-            'arq_irs': self.arq_irs,
-            'arq_iss': self.arq_iss,
-            'arq_session': self.arq_session
-        }
-
-    def initialize_arq_transmission_irs(self, data):
-        if id := self.get_id_from_frame(data):
-            instance = self.initialize_arq_instance()
-            self.states.register_arq_instance_by_id(id, instance)
-            instance['arq_irs'].arq_received_data_channel_opener()
 
 
 
@@ -180,88 +140,3 @@ class DISPATCHER():
 
 
 
-
-
-    def old_process_data(self, bytes_out, freedv, bytes_per_frame: int, snr) -> None:
-        """
-        Process incoming data and decide what to do with the frame.
-
-        Args:
-          bytes_out:
-          freedv:
-          bytes_per_frame:
-          snr:
-
-        Returns:
-
-        """
-        # get frame as dictionary
-        deconstructed_frame = self.frame_factory.deconstruct(bytes_out)
-        frametype = deconstructed_frame["frame_type_int"]
-
-        if frametype not in self.rx_dispatcher:
-            self.log.warning(
-                "[Modem] ARQ - other frame type", frametype=FR_TYPE(frametype).name)
-            return
-
-        # Process frametypes requiring a different set of arguments.
-        if FR_TYPE.BURST_51.value >= frametype >= FR_TYPE.BURST_01.value:
-            self.arq_irs.arq_data_received(
-                deconstructed_frame, bytes_per_frame, snr, freedv
-            )
-
-            # if we received the last frame of a burst or the last remaining rpt frame, do a modem unsync
-            # if self.arq_rx_burst_buffer.count(None) <= 1 or (frame+1) == n_frames_per_burst:
-            #    self.log.debug(f"[Modem] LAST FRAME OF BURST --> UNSYNC {frame+1}/{n_frames_per_burst}")
-            #    self.c_lib.freedv_set_sync(freedv, 0)
-            return
-
-        # TESTFRAMES
-        if frametype == FR_TYPE.TEST_FRAME.value:
-            self.log.debug("[Modem] TESTFRAME RECEIVED", frame=deconstructed_frame)
-            return
-
-        # Process frames "known" by rx_dispatcher
-        # self.log.debug(f"[Modem] {self.rx_dispatcher[frametype][1]} RECEIVED....")
-        self.rx_dispatcher[frametype][0](deconstructed_frame, snr)
-
-    def _initialize_dispatchers(self):
-        """Initializes dispatchers for handling different frame types."""
-        # Dictionary of functions and log messages used in process_data
-        # instead of a long series of if-elif-else statements.
-        self.rx_dispatcher = {
-            FR_TYPE.ARQ_SESSION_OPEN_ACK.value: (
-                self.arq_iss.arq_received_channel_is_open,
-                "ARQ OPEN ACK",
-            ),
-            FR_TYPE.ARQ_SESSION_OPEN.value: (
-                self.initialize_arq_transmission_irs,
-                "ARQ Data Channel Open",
-            ),
-            FR_TYPE.ARQ_CONNECTION_CLOSE.value: (
-                self.arq_session.received_session_close,
-                "ARQ CLOSE SESSION",
-            ),
-            FR_TYPE.ARQ_CONNECTION_HB.value: (
-                self.arq_session.received_session_heartbeat,
-                "ARQ HEARTBEAT",
-            ),
-            FR_TYPE.ARQ_CONNECTION_OPEN.value: (
-                self.arq_session.received_session_opener,
-                "ARQ OPEN SESSION",
-            ),
-            FR_TYPE.ARQ_STOP.value: (self.arq.received_stop_transmission, "ARQ STOP TX"),
-            FR_TYPE.BEACON.value: (self.broadcasts.received_beacon, "BEACON"),
-            FR_TYPE.BURST_ACK.value: (self.arq_iss.burst_ack_nack_received, "BURST ACK"),
-            FR_TYPE.BURST_NACK.value: (self.arq_iss.burst_ack_nack_received, "BURST NACK"),
-            FR_TYPE.CQ.value: (self.broadcasts.received_cq, "CQ"),
-            FR_TYPE.FR_ACK.value: (self.arq_iss.frame_ack_received, "FRAME ACK"),
-            FR_TYPE.FR_NACK.value: (self.arq_iss.frame_nack_received, "FRAME NACK"),
-            FR_TYPE.FR_REPEAT.value: (self.arq_iss.burst_rpt_received, "REPEAT REQUEST"),
-            FR_TYPE.PING_ACK.value: (self.ping.received_ping_ack, "PING ACK"),
-            FR_TYPE.PING.value: (self.ping.received_ping, "PING"),
-            FR_TYPE.QRV.value: (self.broadcasts.received_qrv, "QRV"),
-            FR_TYPE.IS_WRITING.value: (self.broadcasts.received_is_writing, "IS_WRITING"),
-            FR_TYPE.FEC.value: (self.data_broadcasts.received_fec, "FEC"),
-            FR_TYPE.FEC_WAKEUP.value: (self.data_broadcasts.received_fec_wakeup, "FEC WAKEUP"),
-        }
