1f38a59bb834ae0e8de3e7b33b17bb113dbb8e3e
Mashintime <awh@brainless.us>
2023-12-19 20:19:22 -0500

    Fix serial device ignore

--- gui/src/js/deviceFormHelper.ts
+++ gui/src/js/deviceFormHelper.ts
@@ -3,8 +3,6 @@ import { getAudioDevices, getSerialDevices } from "./api";
 let audioDevices = await getAudioDevices();
 let serialDevices = await getSerialDevices();
 
-//Add an ignore option for rig and ptt for transceivers that don't require them
-serialDevices.push({ description: "-- ignore --", port: "ignore" });
 
 //Dummy device data sent if unable to get devices from modem to prevent GUI crash
 const skel = JSON.parse(`
@@ -43,5 +41,14 @@ export function audioOutputOptions() {
 }
 
 export function serialDeviceOptions() {
+  //Return ignore option if no serialDevices
+  if (serialDevices === undefined)
+    return [{ description: "-- ignore --", port: "ignore" }];
+
+  if (serialDevices.findIndex((device) => device.port == 'ignore') == -1) {
+    //Add an ignore option for rig and ptt for transceivers that don't require them
+    serialDevices.push({ description: "-- ignore --", port: "ignore" })
+  }
+    
   return serialDevices;
 }
