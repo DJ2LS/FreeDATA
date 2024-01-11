1a927f66c7708452fdd032625bf93765940c8782
Mashintime <awh@brainless.us>
2024-01-10 19:47:10 -0500

    Fix stop grid widget.

--- gui/src/components/grid/grid_stop.vue
+++ gui/src/components/grid/grid_stop.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import { reactive, ref } from "vue";
 import { setActivePinia } from "pinia";
-import { setConfig } from "../../js/api";
 import pinia from "../../store/index";
 setActivePinia(pinia);
 
@@ -13,11 +11,8 @@ function stopAllTransmissions() {
 }
 </script>
 <template>
-  <div
-    class="d-flex justify-content-center align-items-center object-fill rounded w-100 h-100"
-  >
-    <button
-      class="btn border btn-outline-danger list-group-item w-100 h-100"
+    <a
+      class="btn btn-outline-danger d-flex border justify-content-center align-items-center object-fill rounded w-100 h-100"
       id="stop_transmission_connection"
       data-bs-toggle="tooltip"
       data-bs-trigger="hover"
@@ -26,6 +21,5 @@ function stopAllTransmissions() {
       title="Abort session and stop transmissions"
     >
       <i class="bi bi-sign-stop-fill h1"></i>
-    </button>
-  </div>
+</a>
 </template>
