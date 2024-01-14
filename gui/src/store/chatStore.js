33e2d63de1726ad07ab21384fcc42738b4741527
Mashintime <awh@brainless.us>
2024-01-14 11:47:43 -0500

    Allow sending blank message if an attachment is present

--- gui/src/store/chatStore.js
+++ gui/src/store/chatStore.js
@@ -42,9 +42,9 @@ export const useChatStore = defineStore("chatStore", () => {
   });
   var inputText = ref("");
   var inputFile = ref();
-  var inputFileName = ref();
-  var inputFileType = ref();
-  var inputFileSize = ref();
+  var inputFileName = ref("-");
+  var inputFileType = ref("-");
+  var inputFileSize = ref("-");
 
   var callsign_list = ref();
   var sorted_chat_list = ref();
