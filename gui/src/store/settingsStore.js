9d2332477f757b7e707d5ccd936d1d9600166dfd
DJ2LS <dj2ls@proton.me>
2024-01-23 11:42:47 +0100

    attempt fixing github test

--- gui/src/store/settingsStore.js
+++ gui/src/store/settingsStore.js
@@ -7,12 +7,14 @@ const nconf = require("nconf");
 
 var appDataPath = getAppDataPath();
 var configFolder = path.join(appDataPath, "FreeDATA");
-
 let configFile = "config.json"
+
 const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
 if (isGitHubActions) {
     configFile = "example.json";
+    configFolder = appDataPath;
 }
+
 var configPath = path.join(configFolder, configFile);
 
 console.log("AppData Path:", appDataPath);
