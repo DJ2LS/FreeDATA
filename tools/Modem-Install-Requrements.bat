39b401f8481c688b733f97a7bafc36ce532fbbc4
Mashintime <awh@brainless.us>
2024-02-03 14:46:04 -0500

    Clean up windows batch files out of tools

--- tools/Modem-Install-Requrements.bat
+++
@@ -1,5 +0,0 @@
-REM Place this batch file in FreeData/modem and then run it
-REM ie. c:\FD-Src\modem
-
-python -m pip install -r ..\requirements.txt
-pause
--- tools/Modem-list-audio-devs.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM PLace in modem directory and run to retrieve list of audio devices; you'll need the CRC for the config.ini
-
-python ..\tools\list_audio_devices.py
-
-pause
