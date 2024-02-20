f76dc5da14b5b1c89d404f25d2874e083467048c
DJ2LS <dj2ls@proton.me>
2024-02-20 08:09:05 +0100

    first attempt fixing radio mode related problems by separating parameters

--- modem/server.py
+++ modem/server.py
@@ -233,9 +233,12 @@ def post_modem_send_raw_stop():
 @app.route('/radio', methods=['GET', 'POST'])
 def get_post_radio():
     if request.method in ['POST']:
-        app.radio_manager.set_frequency(request.json['radio_frequency'])
-        app.radio_manager.set_mode(request.json['radio_mode'])
-        app.radio_manager.set_rf_level(int(request.json['radio_rf_level']))
+        if "radio_frequency" in [request.json]:
+            app.radio_manager.set_frequency(request.json['radio_frequency'])
+        if "radio_mode" in [request.json]:
+            app.radio_manager.set_mode(request.json['radio_mode'])
+        if "radio_rf_level" in [request.json]:
+            app.radio_manager.set_rf_level(int(request.json['radio_rf_level']))
 
         return api_response(request.json)
     elif request.method == 'GET':
