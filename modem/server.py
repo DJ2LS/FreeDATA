1ac3185ad39a7b09faa8743e5a708a18dfe0d4c6
DJ2LS <dj2ls@proton.me>
2024-04-12 20:38:33 +0200

    version udpate

--- modem/server.py
+++ modem/server.py
@@ -33,7 +33,7 @@ from schedule_manager import ScheduleManager
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 sock = Sock(app)
-MODEM_VERSION = "0.15.0-alpha"
+MODEM_VERSION = "0.15.1-alpha"
 
 # set config file to use
 def set_config():
