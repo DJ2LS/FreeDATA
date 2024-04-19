460d466a285f77b584fd51d6f3a2d1553b6274d8
DJ2LS <dj2ls@proton.me>
2024-04-19 17:55:36 +0200

    bump pip version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='freedata-server',
-    version='0.15.4a4',
+    version='0.15.4a5',
     packages=find_packages(where='.'),
     package_dir={'': '.'},
     install_requires=required,
