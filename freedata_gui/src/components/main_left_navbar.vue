40ab8a27e8daaa3cf223c99f536a975975300095
codefactor-io <support@codefactor.io>
2024-04-27 20:47:26 +0000

    [CodeFactor] Apply fixes

--- freedata_gui/src/components/main_left_navbar.vue
+++ freedata_gui/src/components/main_left_navbar.vue
@@ -1,28 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 import main_modem_healthcheck from "./main_modem_healthcheck.vue";
 import { getOverallHealth } from "../js/eventHandler.js";
-
 
 const isTextVisible = ref(false); // Initially, the text is invisible
 function toggleTextVisibility() {
   isTextVisible.value = !isTextVisible.value; // Toggle the visibility
 }
-
-
 </script>
 <template>
-
-
   <!-- Button -->
 
   <div class="btn-group" role="group" aria-label="Basic example">
-
-<button class="btn btn-outline-secondary border-0 ms-2 mb-3" type="button" @click="toggleTextVisibility()">
-    <span class=" fw-semibold"><i class="bi bi-text-paragraph"></i></span>
-  </button>
-</div>
-
+    <button
+      class="btn btn-outline-secondary border-0 ms-2 mb-3"
+      type="button"
+      @click="toggleTextVisibility()"
+    >
+      <span class="fw-semibold"><i class="bi bi-text-paragraph"></i></span>
+    </button>
+  </div>
 
   <a
     class="btn border btn-outline-secondary list-group-item rounded-3"
@@ -39,70 +36,66 @@ function toggleTextVisibility() {
     "
     ><i class="h3 bi bi-activity"></i>
     <span class="ms-2" v-if="isTextVisible">Healtcheck</span>
-
   </a>
 
- <div
-  class="list-group bg-body-secondary"
-  id="main-list-tab"
-  role="tablist"
-  style="margin-top: 100px"
-  @click="loadAllData"
->
-
-
-  <a
-    class="list-group-item list-group-item-dark list-group-item-action border-0 rounded-3 mb-2 active"
-    id="list-grid-list"
-    data-bs-toggle="list"
-    href="#list-grid"
-    role="tab"
-    aria-controls="list-grid"
-    title="Grid"
-  >
-    <i class="bi bi-grid h3"></i>
-    <span class="ms-2" v-if="isTextVisible">Home</span>
-  </a>
-
-  <a
-    class="list-group-item list-group-item-dark list-group-item-action border-0 rounded-3 mb-2"
-    id="list-chat-list"
-    data-bs-toggle="list"
-    href="#list-chat"
-    role="tab"
-    aria-controls="list-chat"
-    title="Chat"
-    @click="getFreedataMessages"
-  >
-    <i class="bi bi-chat-text h3"></i>
-    <span class="ms-2" v-if="isTextVisible">RF Chat</span>
-  </a>
-
-  <a
-    class="list-group-item list-group-item-dark list-group-item-action d-none border-0 rounded-3 mb-2"
-    id="list-mesh-list"
-    data-bs-toggle="list"
-    href="#list-mesh"
-    role="tab"
-    aria-controls="list-mesh"
-  >
-    <i class="bi bi-rocket h3"></i>
-    <span class="ms-2" v-if="isTextVisible">Mesh</span>
-  </a>
-
-  <a
-    class="list-group-item list-group-item-dark list-group-item-action border-0 rounded-3 mb-2"
-    id="list-settings-list"
-    data-bs-toggle="list"
-    href="#list-settings"
-    role="tab"
-    aria-controls="list-settings"
-    title="Settings"
+  <div
+    class="list-group bg-body-secondary"
+    id="main-list-tab"
+    role="tablist"
+    style="margin-top: 100px"
     @click="loadAllData"
   >
-    <i class="bi bi-gear-wide-connected h3"></i>
-    <span class="ms-2" v-if="isTextVisible">Settings</span>
-  </a>
-</div>
+    <a
+      class="list-group-item list-group-item-dark list-group-item-action border-0 rounded-3 mb-2 active"
+      id="list-grid-list"
+      data-bs-toggle="list"
+      href="#list-grid"
+      role="tab"
+      aria-controls="list-grid"
+      title="Grid"
+    >
+      <i class="bi bi-grid h3"></i>
+      <span class="ms-2" v-if="isTextVisible">Home</span>
+    </a>
 
+    <a
+      class="list-group-item list-group-item-dark list-group-item-action border-0 rounded-3 mb-2"
+      id="list-chat-list"
+      data-bs-toggle="list"
+      href="#list-chat"
+      role="tab"
+      aria-controls="list-chat"
+      title="Chat"
+      @click="getFreedataMessages"
+    >
+      <i class="bi bi-chat-text h3"></i>
+      <span class="ms-2" v-if="isTextVisible">RF Chat</span>
+    </a>
+
+    <a
+      class="list-group-item list-group-item-dark list-group-item-action d-none border-0 rounded-3 mb-2"
+      id="list-mesh-list"
+      data-bs-toggle="list"
+      href="#list-mesh"
+      role="tab"
+      aria-controls="list-mesh"
+    >
+      <i class="bi bi-rocket h3"></i>
+      <span class="ms-2" v-if="isTextVisible">Mesh</span>
+    </a>
+
+    <a
+      class="list-group-item list-group-item-dark list-group-item-action border-0 rounded-3 mb-2"
+      id="list-settings-list"
+      data-bs-toggle="list"
+      href="#list-settings"
+      role="tab"
+      aria-controls="list-settings"
+      title="Settings"
+      @click="loadAllData"
+    >
+      <i class="bi bi-gear-wide-connected h3"></i>
+      <span class="ms-2" v-if="isTextVisible">Settings</span>
+    </a>
+  </div>
 </template>
