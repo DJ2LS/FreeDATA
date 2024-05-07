4b15eb1673d44c80d6b531a8622052609157e148
Mashintime <awh@brainless.us>
2024-05-07 18:14:41 -0400

    Update disclaimer on exp tab

--- freedata_gui/src/components/settings_exp.vue
+++ freedata_gui/src/components/settings_exp.vue
@@ -9,7 +9,13 @@ import { settingsStore as settings } from "../store/settingsStore.js";
 </script>
 
 <template>
+    <div class="center">
+    <div class="badge text-bg-danger mb-2">
+      
+    </div>
+  </div>
   <div class="input-group input-group-sm mb-1">
+    <label class="input-group-text text-bg-danger w-100"><i class="bi bi-shield-exclamation"></i><strong>&nbsp;WARNING: &nbsp;These features may be incomplete. &nbsp;They are for <u>experienced</u> users ONLY!&nbsp;<i class="bi bi-shield-exclamation"></i></strong></label>
     <label class="input-group-text w-50">Enable MESH protocol</label>
     <label class="input-group-text w-50">
       <div class="form-check form-switch form-check-inline ms-2">
@@ -27,10 +33,5 @@ import { settingsStore as settings } from "../store/settingsStore.js";
       </div>
     </label>
   </div>
-  <div class="center">
-    <div class="badge text-bg-danger">
-      <i class="bi bi-shield-exclamation"></i> These options may not work and
-      are for experienced users only!
-    </div>
-  </div>
+
 </template>
