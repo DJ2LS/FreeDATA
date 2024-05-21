c6f8b01cf500de4f5e620d0d9e3b70cb42ef4c19
Stefan DK5SM <58949681+dk5sm@users.noreply.github.com>
2024-05-21 16:57:22 +0200

    Update and rename Modem-Update-Requrements.bat to Modem-Update-Requirements.bat
    rename file (fix typo) and adjusted path in file remarks

---
+++ tools/Windows/Modem-Update-Requirements.bat
@@ -0,0 +1,5 @@
+REM Place this batch file in FreeData/freedata_server and then run it to update requirements
+REM ie. c:\FD-Src\freedata_server
+
+python -m pip install --upgrade -r ..\requirements.txt
+pause
