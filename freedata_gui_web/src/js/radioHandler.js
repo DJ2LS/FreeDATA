29272053845a28f9d69201d524862d731fc28cd5
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2024-07-22 16:05:54 +0200

    adjusted web gui error handling

--- freedata_gui_web/src/js/radioHandler.js
+++ freedata_gui_web/src/js/radioHandler.js
@@ -9,9 +9,24 @@ const stateStore = useStateStore(pinia);
 import { getRadioStatus } from "./api";
 
 export async function processRadioStatus() {
-  let result = await getRadioStatus();
-  stateStore.mode = result.radio_mode;
-  stateStore.frequency = result.radio_frequency;
-  stateStore.rf_level = Math.round(result.radio_rf_level / 5) * 5; // round to 5er steps
-  stateStore.tuner = result.radio_tuner;
+  try {
+    let result = await getRadioStatus();
+
+   if (!result || typeof result !== 'object') {
+      throw new Error('Invalid radio status');
+    }
+
+    stateStore.mode = result.radio_mode;
+    stateStore.frequency = result.radio_frequency;
+    stateStore.rf_level = Math.round(result.radio_rf_level / 5) * 5; // round to 5er steps
+    stateStore.tuner = result.radio_tuner;
+  } catch (error) {
+    console.error('Error fetching radio status:', error);
+    // Handle the error appropriately
+    // For example, you can set default values or update the UI to indicate an error
+    stateStore.mode = 'unknown';
+    stateStore.frequency = 0;
+    stateStore.rf_level = 0;
+    stateStore.tuner = 'unknown';
+  }
 }
