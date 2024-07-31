770ba6baec8e4c0933f6e02f0314c53ecd871891
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2024-07-31 11:20:14 +0200

    imporved system information

--- freedata_gui/src/components/main_startup_check.vue
+++ freedata_gui/src/components/main_startup_check.vue
@@ -231,10 +231,11 @@
                            data-bs-target="#systemStatusCollapse"
                            data-bs-toggle="collapse"
                            >
+                        </button>
+
                         System Info
                         <!--
                         <button type="button" class="btn btn-sm btn-outline-secondary" @click="copyToClipboard">Copy</button>
-                        </button>
                         -->
                      </h2>
                      <div id="systemStatusCollapse" class="accordion-collapse collapse">
