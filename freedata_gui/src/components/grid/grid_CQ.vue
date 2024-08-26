f480032c9aa762433d96932bfcdc9ee9469fde6a
DJ2LS <dj2ls@proton.me>
2024-08-26 10:52:51 +0200

    CQ button cooldown

--- freedata_gui/src/components/grid/grid_CQ.vue
+++ freedata_gui/src/components/grid/grid_CQ.vue
@@ -2,12 +2,26 @@
 import { setActivePinia } from 'pinia';
 import pinia from '../../store/index';
 setActivePinia(pinia);
+import { ref } from "vue";
+
 
 import { sendModemCQ } from '../../js/api.js';
+const isCQButtonDisabled = ref(false);
 
-function handleCQCall() {
-  sendModemCQ();
+// Function to send CQ and handle button disable and cooldown
+async function handleCQCall() {
+  isCQButtonDisabled.value = true;
+
+  // Send CQ message
+  await sendModemCQ();
+
+  // Wait for 6 seconds (cooldown period)
+  setTimeout(() => {
+    isCQButtonDisabled.value = false;
+  }, 6000);
 }
+
+
 </script>
 
 <template>
@@ -17,7 +31,8 @@ function handleCQCall() {
       @click="handleCQCall"
       title="Send a CQ call!"
     >
-      CQ
+      <span v-if="!isCQButtonDisabled">CQ</span>
+      <span v-else>...</span>
     </a>
   </div>
 </template>
