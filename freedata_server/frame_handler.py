f50cfec67c20f163cb67c16042e762241a846da3
DJ2LS <dj2ls@proton.me>
2024-09-13 20:26:04 +0200

    try to find station info in database

--- freedata_server/frame_handler.py
+++ freedata_server/frame_handler.py
@@ -5,6 +5,8 @@ import structlog
 import time
 from codec2 import FREEDV_MODE
 from message_system_db_manager import DatabaseManager
+from message_system_db_station import DatabaseManagerStations
+
 import maidenhead
 
 TESTMODE = False
@@ -127,6 +129,14 @@ class FrameHandler():
             return
 
         dxgrid = frame.get('gridsquare', "------")
+        #try to find station info in database
+        try:
+            station = DatabaseManagerStations(self.event_manager).get_station(frame['origin'])
+            if station and "gridsquare" in station["location"]:
+                dxgrid = station["location"]["gridsquare"]
+        except Exception:
+            pass
+
         # Initialize distance values
         distance_km = None
         distance_miles = None
