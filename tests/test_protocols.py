4c84391fd4746060f89283f6a684a204f48f1ceb
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2025-01-22 09:34:08 +0100

    fixing test

--- tests/test_protocols.py
+++ tests/test_protocols.py
@@ -27,7 +27,7 @@ class TestProtocols(unittest.TestCase):
         cls.event_queue = queue.Queue()
         cls.event_manager = EventManager([cls.event_queue])
 
-        cls.radio_manager = RadioManager(cls.config, cls.state_manager, cls.event_manager)
+        cls.radio_manager = RadioManager(cls.config, cls.state_manager, cls.event_manager, None)
         cls.modem_transmit_queue = queue.Queue()
 
         cls.modem = modem.RF(cls.config, cls.event_queue, queue.Queue(), queue.Queue(), cls.state_manager, cls.radio_manager)
