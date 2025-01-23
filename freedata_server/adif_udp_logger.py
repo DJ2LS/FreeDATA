4ba1624766e0244dbdd73811a1a06bbf3e8873d4
LA3QMA <kai.gunter.brandt@gmail.com>
2025-01-23 12:37:40 +0100

    If there is no gridsquare fix this so that it follows the ADIF standard.

--- freedata_server/adif_udp_logger.py
+++ freedata_server/adif_udp_logger.py
@@ -1,5 +1,5 @@
 import socket
-
+import re
 
 
 def send_adif_qso_data(config, adif_data):
@@ -16,6 +16,11 @@ def send_adif_qso_data(config, adif_data):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
+
+        # If there is no gridsquare fix this so that it follows the ADIF standard.
+        #FIXME this check should probably be in message_system_db_station/model.py so this should be considered as a quick fix
+        adif_data = re.sub(r"<GRIDSQUARE:4>----", "<GRIDSQUARE:0>", adif_data)
+
         # Send the ADIF data to the server
         sock.sendto(adif_data.encode('utf-8'), (adif_log_host, adif_log_port))
         print(f"ADIF QSO data sent to {adif_log_host}:{adif_log_port}", adif_data.encode('utf-8'))
