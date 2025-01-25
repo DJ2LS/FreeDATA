d794f75199f0cc3db18e6b5f022eef5721774796
LA3QMA <kai.gunter.brandt@gmail.com>
2025-01-25 09:53:58 +0100

    corrected the keys for wavelog

--- freedata_server/config.py
+++ freedata_server/config.py
@@ -76,10 +76,10 @@ class CONFIG:
         },
         'MESSAGES': {
             'enable_auto_repeat': bool,
-            'enable_adif_udp': bool,
+            'enable_adif': bool,
             'adif_log_host': str,
             'adif_log_port': int,
-            'enable_wavelog': bool,
+            'wavelog_enable': bool,
             'wavelog_host': str,
             'wavelog_api_key': str,
         },
