f0ee488c785755630e726ba10a6621143cfcaa2a
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2025-02-24 18:13:21 +0100

    test with com0com

--- freedata_server/serial_ports.py
+++ freedata_server/serial_ports.py
@@ -1,9 +1,17 @@
 import serial.tools.list_ports
+import list_ports_winreg
 import helpers
+import sys
+
+
 def get_ports():
 
     serial_devices = []
-    ports = serial.tools.list_ports.comports(include_links=False)
+    if sys.platform == 'win32':
+        ports = serial.tools.list_ports.comports(include_links=False)
+    else:
+        ports = list_ports_winreg.comports(include_links=False)
+
     for port, desc, hwid in ports:
         # calculate hex of hwid if we have unique names
         crc_hwid = helpers.get_crc_16(bytes(hwid, encoding="utf-8"))
