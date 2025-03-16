e3bf1ec5e5397d70613ff5cb7fbc9feeaf0e5619
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2025-03-16 09:30:28 +0100

    bump version info
    0.17.0-beta

--- freedata_server/constants.py
+++ freedata_server/constants.py
@@ -1,7 +1,7 @@
 # Module for saving some constants
 CONFIG_ENV_VAR = 'FREEDATA_CONFIG'
 DEFAULT_CONFIG_FILE = 'config.ini'
-MODEM_VERSION = "0.17.0-alpha"
+MODEM_VERSION = "0.17.0-beta"
 API_VERSION = 3
 LICENSE = 'GPL3.0'
 DOCUMENTATION_URL = 'https://wiki.freedata.app'
