d4ccda12f89289dc2690d218eb2f1e028a3432d9
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2025-04-02 11:56:02 +0200

    adjusted linting packages

---
+++ freedata_gui/eslint.config.js
@@ -0,0 +1,19 @@
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+
+export default [
+    ...pluginVue.configs['flat/base'],
+    ...pluginVue.configs['flat/recommended'],
+  {
+    ignores: ["**/*.config.js", "!**/eslint.config.js", "**/src/locales/**", "**/node_modules/**"],
+    rules: {
+      'vue/no-unused-vars': 'error'
+    },
+    languageOptions: {
+      //sourceType: 'module',
+      globals: {
+        ...globals.browser
+      }
+    }
+  }
+]
