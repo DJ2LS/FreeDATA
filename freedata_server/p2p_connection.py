ec4a8c90e2b86c030baaf99c21704b1bf9d1b352
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2025-05-05 09:55:13 +0200

    work on heartbeat

--- freedata_server/p2p_connection.py
+++ freedata_server/p2p_connection.py
@@ -326,15 +326,11 @@ class P2PConnection:
     def transmit_heartbeat_ack(self):
         print("transmit heartbeat ack")
 
-        if self.p2p_data_tx_queue.empty():
-            self.flag_buffer_empty = True
-
-
+        self.flag_buffer_empty = self.p2p_data_tx_queue.empty()
         self.last_data_timestamp = time.time()
         heartbeat_ack = self.frame_factory.build_p2p_connection_heartbeat_ack(self.session_id, flag_buffer_empty=self.flag_buffer_empty,flag_announce_arq=self.flag_announce_arq)
         print(heartbeat_ack)
         self.launch_twr_irs(heartbeat_ack, self.ENTIRE_CONNECTION_TIMEOUT, mode=FREEDV_MODE.signalling)
-
 
     def received_heartbeat(self, frame):
         print("received heartbeat...")
@@ -363,9 +359,13 @@ class P2PConnection:
         print("received heartbeat ack from IRS...")
         buffer_empty_flag = frame.get('flag', {}).get('BUFFER_EMPTY', False)
         announce_arq_flag = frame.get('flag', {}).get('ANNOUNCE_ARQ', False)
+
         if buffer_empty_flag:
             print("other stations buffer is empty. We can become data master now")
             self.is_Master = True
+        else:
+            print("other station has data to be sent...")
+            self.is_Master = False
 
         if announce_arq_flag:
             print("other station announced arq, changing state")
