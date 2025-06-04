e73a8ca6f6d568d836ddc23ddd43daa392a2f842
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2025-06-04 20:27:39 +0200

    adjusted streaming

--- freedata_gui/src/components/grid/grid_audio.vue
+++ freedata_gui/src/components/grid/grid_audio.vue
@@ -1,29 +1,33 @@
 <script setup>
 import { setActivePinia } from 'pinia';
+import { ref } from 'vue';
+
 import pinia from '../../store/index';
 setActivePinia(pinia);
 
 import { useAudioStore } from '@/store/audioStore';
 const audio = useAudioStore(pinia);
 
+
+
+
 var audioCtx = null;
-var isPlaying = false;
+var isPlaying = ref(false);
 
 function playRxStream() {
-  if (isPlaying) return;
+  if (isPlaying.value) return;
+
 
   const SAMPLE_RATE = 8000;
-  const BLOCK_SIZE = 300;
-  const BLOCK_DURATION_MS = (BLOCK_SIZE / SAMPLE_RATE) * 1000;  // ≈75ms
 
   audioCtx = new (window.AudioContext || window.webkitAudioContext)({ sampleRate: SAMPLE_RATE });
-  let scheduledTime = audioCtx.currentTime;
-  isPlaying = true;
+  //let scheduledTime = audioCtx.currentTime;
+  isPlaying.value = true;
 
-  console.log("▶️ Echtzeit-Wiedergabe gestartet");
+  console.log("audio playback");
 
   function loop() {
-    if (!isPlaying) return;
+    if (!isPlaying.value) return;
 
     const block = audio.getNextBlock();
     if (block) {
@@ -35,7 +39,6 @@ function playRxStream() {
       source.buffer = buffer;
       source.connect(audioCtx.destination);
       source.start(0);
-
     } else {
       //console.warn("⛔ Audio buffer underrun");
     }
@@ -52,14 +55,38 @@ function playRxStream() {
 
 function stopRxStream() {
   if (audioCtx) {
-    isPlaying = false;
+    isPlaying.value = false;
     audioCtx.close();
     console.log("Playback stopped");
   }
 }
+
+
+function toggleRxStream() {
+  if (isPlaying.value) {
+    stopRxStream()
+  } else {
+    playRxStream()
+  }
+}
+
 </script>
 
 <template>
-  <button @click="playRxStream">Start Audio</button>
-  <button @click="stopRxStream">Stop Audio</button>
+
+   <div class="card h-100">
+    <div class="card-header">
+
+      <strong>{{ $t('grid.components.audiostream') }}</strong>
+    </div>
+    <div class="card-body overflow-auto m-0" style="align-items: start">
+ <button :class="isPlaying ? 'btn btn-sm btn-danger' : 'btn btn-sm btn-success'" @click="toggleRxStream">
+            <i :class="isPlaying ? 'bi bi-stop-fill' : 'bi bi-play-fill'"/>&nbsp;
+   {{isPlaying ? 'Stop' : 'Start'}}
+          </button>
+
+    </div>
+  </div>
+
+
 </template>
