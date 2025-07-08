391287bd90acc9788daafc3578b25bfe6c53dc95
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2025-07-08 11:18:10 +0200

    adjusted backoff intervall

--- freedata_server/message_system_db_broadcasts.py
+++ freedata_server/message_system_db_broadcasts.py
@@ -349,7 +349,7 @@ class DatabaseManagerBroadcasts(DatabaseManager):
                 print("timestamp", msg.timestamp)
                 print("received_at", msg.received_at)
                 if msg.nexttransmission_at and now < msg.nexttransmission_at:
-                    self.log(f"Skip {msg.id}: wait until {msg.nexttransmission_at.isoformat()}")
+                    self.log(f"Skip {msg.id}: wait until {int(msg.nexttransmission_at).isoformat()}")
 
                     continue
 
