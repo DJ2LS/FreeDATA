987539ce13eb4a760fcbff188db60c0a6045289c
dj2ls <lanbg.simon@gmx.net>
2026-01-08 17:29:21 +0100

    bump version

--- freedata_server/constants.py
+++ freedata_server/constants.py
@@ -1,8 +1,8 @@
 # Module for saving some constants
 CONFIG_ENV_VAR = 'FREEDATA_CONFIG'
 DEFAULT_CONFIG_FILE = 'config.ini'
-MODEM_VERSION = "0.18.0-experimental"
-API_VERSION = 3
+MODEM_VERSION = "0.19.0"
+API_VERSION = 4
 ARQ_PROTOCOL_VERSION = 1
 LICENSE = 'GPL3.0'
 DOCUMENTATION_URL = 'https://wiki.freedata.app'
