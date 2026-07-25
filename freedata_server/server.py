202abeff421998409304da1c4ffb16a115870d8c
LA3QMA <kai.gunter.brandt@gmail.com>
2026-07-25 12:25:06 +0200

    need to find config.ini.example and create in correct folder

--- freedata_server/server.py
+++ freedata_server/server.py
@@ -45,13 +45,15 @@ def resolve_config_path() -> str:
 
     if not os.path.exists(candidate):
         # We cannot log to file yet since we don't know the directory; write to stderr.
-        template = os.path.join(os.path.dirname(__file__), "config.ini.example")
+
         try:
             os.makedirs(os.path.dirname(candidate), exist_ok=True)
+            template = os.path.join(os.path.dirname(__file__), "config.ini.example")
             if os.path.isfile(template):
                 shutil.copyfile(template, candidate)
                 sys.stderr.write(f"[INFO] No config found - created a default one at: {candidate}\n")
             else:
+                open(candidate, "w").close()
                 sys.stderr.write(
                     f"[FATAL] Config file not found and no template available to create one: {candidate}\n"
                 )
